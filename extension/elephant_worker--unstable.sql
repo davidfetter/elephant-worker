38ca9d49b8fcd4b090f2677d9f9c5eee864893ff
Feike Steenbergen <feikesteenbergen@gmail.com>
2015-08-13 14:07:25 +0200

    Remove the parse_cronfield function and inline it into parse_crontab

--- extension/elephant_worker--unstable.sql
+++ extension/elephant_worker--unstable.sql
@@ -17,74 +17,17 @@ BEGIN
     END LOOP;
 END;
 $$;
-CREATE FUNCTION @extschema@.parse_cronfield (cronfield text, minvalue int, maxvalue int)
-RETURNS int []
-RETURNS NULL ON NULL INPUT
-LANGUAGE plpgsql
-AS
-$BODY$
-DECLARE
-    entry text;
-    cronvalues int [] := ARRAY[]::int[];
-    -- Example entries : 0-4,5-9/3,8   */3    11-12
-    entry_regexp text := '^(\*|(\d{1,2})(-(\d{1,2}))?)(\/(\d{1,2}))?$';
-    entry_groups text [];
-    min int;
-    max int;
-    step int;
-    result int[];
-BEGIN
-    FOREACH entry IN ARRAY string_to_array(cronfield, ',')
-    LOOP
-        entry_groups := regexp_matches(entry, entry_regexp);
-        min := entry_groups[2];
-        step := coalesce(entry_groups[6]::int,1);
-        IF entry_groups[1] = '*' THEN
-            min := minvalue;
-            max := maxvalue;
-        ELSE
-            max := coalesce(entry_groups[4]::int,min);
-        END IF;
-
-        IF max < min OR max > maxvalue OR min < minvalue THEN
-            RAISE SQLSTATE '22023' USING
-                MESSAGE = 'Invalid crontab parameter.',
-                DETAIL  = format('Range start: %s (%s), End range: %s (%s), Step: %s for crontab field: %s', min, minvalue, max, maxvalue, step, cronfield),
-                HINT    = 'Ensure range is ascending and that the ranges is within allowed bounds';
-        END IF;
-
-        cronvalues := cronvalues || array(SELECT generate_series(min, max, step));
-    END LOOP;
-
-    result := array(SELECT DISTINCT * FROM unnest(cronvalues) ORDER BY 1);
-    IF result = '{}'::int[] THEN
-        RETURN null;
-    END IF;
-
-    RETURN result;
-END;
-$BODY$
-SECURITY INVOKER
-IMMUTABLE;
-
-COMMENT ON FUNCTION @extschema@.parse_cronfield (text, int, int) IS
-'Parses a single crontab field. Raises an error if the field is definitely invalid, or null when it is unknown';
-
--- Parsing a crontab entry seems tedious, but it is usefull to do
--- it as part of a check constraint. We ensure that there are only
--- valid entries in the job table.
 -- Main source for decicions is man 5 crontab
-CREATE FUNCTION @extschema@.parse_crontab (schedule text, OUT minute int [], OUT hour int [], OUT dom int[], OUT month int[], OUT dow int[])
+CREATE OR REPLACE FUNCTION @extschema@.parse_crontab (schedule text, OUT minute int [], OUT hour int [], OUT dom int[], OUT month int[], OUT dow int[], OUT timezone text)
 LANGUAGE plpgsql
 AS
 $BODY$
 DECLARE
-    entries text [] := regexp_split_to_array(schedule, '\s+');
-    entry   text;
-    empty int[] := '{}'::int[];
+    entries         text [] := regexp_split_to_array(schedule, '\s+');
+    entry           text;
 BEGIN
     -- Allow some named entries, we transform them into the documented equivalent
-    IF array_length (entries, 1) <> 5 THEN
+    IF array_length (entries, 1) < 5 THEN
         IF array_length (entries, 1) = 1 THEN
             IF entries[1] = '@yearly' OR entries[1] = '@annually' THEN
                 entries := ARRAY['0','0','1','1','*'];
@@ -102,23 +45,65 @@ BEGIN
         ELSE
             RETURN;
         END IF;
+    ELSIF array_length(entries, 1) > 5 THEN
+        timezone := entries[6];
     END IF;
 
-    minute := @extschema@.parse_cronfield(entries[1],0,59);
-    hour   := @extschema@.parse_cronfield(entries[2],0,23);
-    dom    := @extschema@.parse_cronfield(entries[3],1,31);
-    month  := @extschema@.parse_cronfield(entries[4],1,12);
-    dow    := @extschema@.parse_cronfield(entries[5],0,7);
+    -- We parse the 5 groups of crontab in a loop
+    DECLARE
+        cronfield_entry_groups text [];
+        cronfield_values int [];
+        entry_minvalues int [] := ARRAY[0,0,1,1,0];
+        entry_maxvalues int [] := ARRAY[59,23,31,12,7];
+        cronfield_entry_regexp text := '^(\*|(\d{1,2})(-(\d{1,2}))?)(\/(\d{1,2}))?$';
+        cronfield_entry text;
+        maxvalue int;
+        minvalue int;
+        min int;
+        max int;
+        step int;
+        result int[];
+    BEGIN
+        FOR i IN 1..5 LOOP
+            cronfield_values := '{}'; 
+            minvalue         := entry_minvalues[i];
+            maxvalue         := entry_maxvalues[i];
+            FOREACH cronfield_entry IN ARRAY string_to_array(entries[i], ',')
+            LOOP
+                cronfield_entry_groups := regexp_matches(cronfield_entry, cronfield_entry_regexp);
+                min := cronfield_entry_groups[2];
+                step := coalesce(cronfield_entry_groups[6]::int,1);
+                IF cronfield_entry_groups[1] = '*' THEN
+                    min := minvalue;
+                    max := maxvalue;
+                ELSE
+                    max := coalesce(cronfield_entry_groups[4]::int,min);
+                END IF;
 
-    -- if any entry is null, the crontab is invalid
-    IF minute IS NULL OR hour IS NULL OR month IS NULL OR dom IS NULL or dow IS NULL THEN
-        minute := null;
-        hour   := null;
-        month  := null;
-        dom    := null;
-        dow    := null;
-        RETURN;
-    END IF;
+                IF max < min OR max > maxvalue OR min < minvalue THEN
+                    RAISE SQLSTATE '22023' USING
+                        MESSAGE = 'Invalid crontab parameter.',
+                        DETAIL  = format('Range start: %s (%s), End range: %s (%s), Step: %s for crontab field: %s', min, minvalue, max, maxvalue, step, cronfield),
+                        HINT    = 'Ensure range is ascending and that the ranges is within allowed bounds';
+                END IF;
+
+                cronfield_values := cronfield_values || array(SELECT generate_series(min, max, step));
+            END LOOP;
+
+            IF    i = 1 THEN
+                minute := cronfield_values;
+            ELSIF i = 2 THEN
+                hour := cronfield_values;
+            ELSIF i = 3 THEN
+                dom := cronfield_values;
+            ELSIF i = 4 THEN
+                month := cronfield_values;
+            ELSIF i = 5 THEN
+                dow := cronfield_values;
+            END IF;
+
+        END LOOP;
+    END; 
 
     -- Convert day 7 to day 0 (Sunday)
     dow :=  array(SELECT DISTINCT unnest(dow)%7 ORDER BY 1);
@@ -132,6 +117,16 @@ BEGIN
     END IF;
     IF entries[3] = '*' AND entries[5] != '*' THEN
         dom := '{}'::int[];
+    END IF;
+
+    -- if any entry is null, the crontab is invalid
+    IF minute IS NULL OR hour IS NULL OR dom IS NULL OR month IS NULL OR dow IS NULL THEN
+        minute := null;
+        hour   := null;
+        dom    := null;
+        month  := null;
+        dow    := null;
+        timezone := null;
     END IF;
 
     RETURN;
